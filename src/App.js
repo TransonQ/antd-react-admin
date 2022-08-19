f4959c6d093a78564888cc9c8ab14c6a1b160734
Transon Quan <68772719+quanscheng@users.noreply.github.com>
2022-08-19 14:35:11 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.less";
+
+import { Button } from "antd";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          antd react 18
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button>antd button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
